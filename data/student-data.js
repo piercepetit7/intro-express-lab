b33839efda52adde4c2f01dbf17487faf5f18333
Pierce Petit <ppetit2496@gmail.com>
2022-06-13 19:37:57 -0500

    finish lab

---
+++ data/student-data.js
@@ -0,0 +1,14 @@
+const students = [
+    {name: 'Bob', enrolled: true, _id: 1001},
+    {name: 'Jim', enrolled: true, _id: 1002},
+    {name: 'Karen', enrolled: false, _id: 1003},
+    {name: 'Lauren', enrolled: true, _id: 1004},
+    {name: 'Skip', enrolled: false, _id: 1005},
+    {name: 'Doug', enrolled: false, _id: 1006},
+    {name: 'Pierce', enrolled: true, _id: 1007},
+
+]
+
+export { 
+    students
+}
